3b3f40076ec730a77c77c67ff158a4eda797c7fc
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-04-14 20:58:38 -0300

    done: push to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    const newTask = {
+      id: Math.random().toString(10).substring(7),
+      title: taskTitle,
+    };
+
+    if (taskTitle) {
+      const tasksCopy = [...tasks];
+      tasksCopy.push(newTask);
+      setTasks(tasksCopy);
+      setTaskTitle("");
+    }
+  }
+
+  function deleteTask(id) {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Digite a tarefa..."
+          value={taskTitle}
+          onChange={(e) => setTaskTitle(e.target.value)}
+        />
+        <button type="button" onClick={addTask}>
+          Salvar
+        </button>
+      </div>
+      <div className="tasks">
+        {tasks.map((task) => (
+          <div className="task" key={task.id}>
+            <p className="task-title">{task.title}</p>
+            <button type="button" onClick={() => deleteTask(task.id)}>
+              X
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
